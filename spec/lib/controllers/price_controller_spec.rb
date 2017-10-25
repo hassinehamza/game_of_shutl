8d9f2c80a9ac9a0c165652e62f33a2d81f68b3f2
hamza.hassine <hamza.hassine@supcom.tn>
2017-10-25 04:37:53 +0200

    orginize test files

--- spec/lib/controllers/price_controller_spec.rb
+++ spec/lib/controllers/price_controller_spec.rb
@@ -2,17 +2,23 @@ require 'spec_helper'
 
 describe GameOfShutl::Price do
 
-    let(:price) do
-      GameOfShutl::Price.new
-    end
+  let(:price) do
+    GameOfShutl::Price.new
+  end
 
-  describe 'basic price' do
-    it 'converts the postcodes to base-32 and subtracts delivery from pickup' do
+  describe 'calculatBasicPrice method' do
+    it 'converts the postcodes to base-32 and subtracts delivery from pickup and calculate basic price' do
 
         pickup_postcode =  'SW1A 1AA'
         delivery_postcode = 'EC2A 3LT'
 
         expect(price.calculBasicPrice(pickup_postcode, delivery_postcode)).to eq 679
+    end
+    it 'calculate variable price by distance' do
+      pickup_postcode =  'AL1 5WD'
+      delivery_postcode = 'EC2A 3LT'
+
+      expect(price.calculBasicPrice(pickup_postcode, delivery_postcode)).to eq 656
     end
   end
 end
